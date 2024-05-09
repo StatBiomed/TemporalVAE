e1570437d6b3f3f7b58f476d277612ab56307108
awa121 <yijun19@mails.jlu.edu.cn>
2024-05-09 10:23:49 +0800

    fig2

---
+++ model_master/base.py
@@ -0,0 +1,49 @@
+from .types_ import *
+from torch import nn
+from abc import abstractmethod
+
+class BaseVAE(nn.Module):
+    
+    def __init__(self) -> None:
+        super(BaseVAE, self).__init__()
+        self.init_weights()
+
+    def encode(self, input: Tensor) -> List[Tensor]:
+        raise NotImplementedError
+
+    def decode(self, input: Tensor) -> Any:
+        raise NotImplementedError
+
+    def sample(self, batch_size:int, current_device: int, **kwargs) -> Tensor:
+        raise NotImplementedError
+
+    def generate(self, x: Tensor, **kwargs) -> Tensor:
+        raise NotImplementedError
+
+    def init_weights(self):#2023-07-05 16:56:04
+        def init_module(module):
+            if isinstance(module, nn.Linear):
+                nn.init.xavier_uniform_(module.weight)
+                nn.init.constant_(module.bias, 0.0)
+            elif isinstance(module, nn.BatchNorm1d):
+                nn.init.constant_(module.weight, 1.0)
+                nn.init.constant_(module.bias, 0.0)
+            elif isinstance(module, nn.Conv2d):
+                nn.init.xavier_uniform_(module.weight)
+                nn.init.constant_(module.bias, 0.0)
+            elif isinstance(module, nn.BatchNorm2d):
+                nn.init.constant_(module.weight, 1.0)
+                nn.init.constant_(module.bias, 0.0)
+
+        self.apply(init_module)
+    @abstractmethod
+    def forward(self, *inputs: Tensor) -> Tensor:
+        pass
+
+    @abstractmethod
+    def loss_function(self, *inputs: Any, **kwargs) -> Tensor:
+        pass
+
+
+
+
