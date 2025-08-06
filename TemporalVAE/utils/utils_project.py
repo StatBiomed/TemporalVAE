992220e9d21d841fe3fb61c6c068bcab5f64e0ac
awa121 <49545852+awa121@users.noreply.github.com>
2025-08-07 00:40:16 +0800

    embryo demo

--- TemporalVAE/utils/utils_project.py
+++ TemporalVAE/utils/utils_project.py
@@ -3536,3 +3536,229 @@ def voteScore_genePerturbation(cell_df, perturb_df, top_gene_num, predictedTime_
     all_top_columns = [col for sublist in top_columns_per_row for col in sublist]
     column_counts = pd.Series(all_top_columns).value_counts()
     return column_counts
+def from_adata_randomSelect_cells_equityTimePoint(adata_mu_reference, random_select_n_timePoint=200, random_seed=0):
+    from collections import Counter
+    # Randomly select 200 variables (genes) from adata
+    np.random.seed(random_seed)
+
+    # Ensure 'time' is categorical
+    time_categories = adata_mu_reference.obs['time'].astype('category').cat.categories
+
+    # Initialize a list to store selected cell indices
+    selected_cell_indices = []
+
+    # Iterate over each time point
+    for time_point in time_categories:
+        # Get indices of cells for the current time point
+        time_mask = adata_mu_reference.obs['time'] == time_point
+        cell_indices = np.where(time_mask)[0]  # or use adata_mu_reference.obs.index[time_mask]
+
+        # Determine how many cells to sample (min between 200 and available cells)
+        n_cells = min(random_select_n_timePoint, len(cell_indices))
+
+        # Randomly select cells without replacement
+        selected_indices = np.random.choice(cell_indices, size=n_cells, replace=False)
+        selected_cell_indices.extend(selected_indices)
+
+    # Subset the AnnData object using the selected indices
+    adata_subset = adata_mu_reference[selected_cell_indices, :].copy()  # .copy() to avoid warnings
+
+    # Optional: Reset indices if needed
+    adata_subset.obs.reset_index(drop=True, inplace=True)
+    print(f"Before random select:\n\t"
+          f"{Counter(adata_mu_reference.obs['dataset_label'])}\n\t "
+          f"{Counter(adata_mu_reference.obs['time'])}\n\t"
+          f"{Counter(adata_mu_reference.obs['cell_type'])}\n"
+          f"After random select:"
+          f"{Counter(adata_subset.obs['dataset_label'])}\n \t "
+          f"{Counter(adata_subset.obs['time'])}\n\t"
+          f"{Counter(adata_subset.obs['cell_type'])}")
+    return adata_subset
+def queryOneDataset_referenceOn6Datasets_humanEmbryo(test_donor,
+                                                     cell_time, sc_expression_df,
+                                                     time_standard_type, label_dic, batch_dic,
+                                                     runner, experiment, adata_mu_reference, _logger,
+                                                     save_path,
+                                                     umap_reducer,
+                                                     special_file_name='',
+
+                                                     # umap_n_neighbors=50,
+                                                     # umap_space_withSubsetRef=False,random_select_n_timePoint=200,
+                                                     ):
+    from collections import Counter
+    # ---- set query data and predict on query data
+    cell_time_dic = dict(zip(cell_time.index, cell_time['time']))
+    sc_expression_test = sc_expression_df.loc[cell_time.index[cell_time['dataset_label'] == test_donor]]
+    x_sc_test = torch.tensor(sc_expression_test.values, dtype=torch.get_default_dtype()).t()
+    y_time_test = x_sc_test.new_tensor(np.array(sc_expression_test.index.map(cell_time_dic) * 100).astype(int))
+    try:
+        y_time_nor_test, label_dic = trans_time(y_time_test, time_standard_type, label_dic_train=label_dic)
+    except:
+        print("error")
+    x_sc_test = torch.tensor(sc_expression_test.values, dtype=torch.get_default_dtype()).t()
+    donor_index_test = x_sc_test.new_tensor(
+        [int(batch_dic[cell_time.loc[_cell_name]['dataset_label']]) for _cell_name in sc_expression_test.index.values])
+    test_data = [[x_sc_test[:, i], y_time_nor_test[i], donor_index_test[i]] for i in range(x_sc_test.shape[1])]
+    from TemporalVAE.model_master.dataset import SupervisedVAEDataset_onlyPredict
+    data_test = SupervisedVAEDataset_onlyPredict(predict_data=test_data, predict_batch_size=len(test_data))
+    test_result = runner.predict(experiment, data_test)
+    test_clf_result, test_mu_result, test_latent_log_var_result = test_result[0][0], test_result[0][1], \
+        test_result[0][2]
+    if test_clf_result.shape[1] == 1:
+        # time is continues, supervise_vae_regressionclfdecoder  supervise_vae_regressionclfdecoder_of_sublatentspace
+        _logger.info("predicted time of test donor is continuous.")
+        import pandas as pd
+        test_clf_result = pd.DataFrame(data=np.squeeze(test_clf_result, axis=1), index=sc_expression_test.index, columns=["pseudotime"])
+
+    cell_time_tyser = cell_time.loc[sc_expression_test.index]
+    cell_time_tyser["predicted_time"] = test_clf_result.apply(denormalize, args=(min(label_dic.keys()) / 100, max(label_dic.keys()) / 100,
+                                                                                 min(label_dic.values()), max(label_dic.values())))
+    cell_time_tyser = cell_time_tyser[["time", "predicted_time", "dataset_label", "day", "cell_type"]]
+    adata_mu_query = anndata.AnnData(X=test_mu_result.cpu().numpy(), obs=cell_time_tyser)
+    adata_mu_query.obs['data_type'] = test_donor
+    # ----
+
+    # ---- combine low-dim representation of reference and query data
+    adata_combined = anndata.concat([adata_mu_reference.copy(), adata_mu_query.copy()], axis=0)
+
+    adata_combined.obs["cell_typeMask4dataset"] = adata_combined.obs.apply(lambda row: 'L & M & P & Z & Xiao & C' if row['dataset_label'] != test_donor else row['cell_type'],
+                                                                           axis=1)
+    adata_combined.obs["cell_typeMaskTyser"] = adata_combined.obs.apply(lambda row: test_donor if row['dataset_label'] == test_donor else row['cell_type'], axis=1)
+    # ----
+
+    # ---- plot predicted violin images.
+    plot_violin_240223(adata_combined.obs.copy(),
+                       save_path,
+                       x_attr="time",
+                       y_attr="predicted_time",
+                       special_file_name=f"queryOn{test_donor}_violinAll{special_file_name}")
+    plot_violin_240223(adata_combined.obs.loc[adata_combined.obs.index[adata_combined.obs['dataset_label'] == test_donor]].copy(),
+                       save_path,
+                       x_attr="time",
+                       y_attr="predicted_time",
+                       special_file_name=f"queryOn{test_donor}_violoinTest{special_file_name}")
+    plot_violin_240223(adata_combined.obs.loc[adata_combined.obs.index[adata_combined.obs['dataset_label'] != test_donor]].copy(),
+                       save_path,
+                       x_attr="time",
+                       y_attr="predicted_time",
+                       special_file_name=f"queryOn{test_donor}_violoinTrain{special_file_name}")
+
+    # ---- 1 method: mapping tyser data to other 4 dataset's umap, just use different umap model
+    # sc.pp.neighbors(adata_mu_query, n_neighbors=50, n_pcs=20)
+    # sc.tl.umap(adata_mu_query, min_dist=0.75)
+    # ----2 method mapping tyser data to other 4 dataset's umap, use same umap model by 4 dataset,
+    # Create a UMAP model instance
+    # import umap
+    # reducer = umap.UMAP(n_neighbors=umap_n_neighbors, min_dist=0.75, n_components=2, random_state=0)
+    # reducer = umap.UMAP(n_neighbors=50, min_dist=0.75, n_components=2, random_state=101)
+    # reducer = umap.UMAP(n_neighbors=15, min_dist=0.75, n_components=2, random_state=10)
+    print(f"{Counter(adata_mu_reference.obs['dataset_label'])}")
+    print(f"{Counter(adata_mu_reference.obs['time'])}")
+
+    # adata_mu_reference.obsm['X_umap'] = reducer.fit_transform(adata_mu_reference.X)
+    adata_mu_query.obsm['X_umap'] = umap_reducer.transform(adata_mu_query.X)
+
+    ### ---------------- Plot images ---------------
+    reference_dataset_str = '&'.join(adata_combined.obs['dataset_label'].unique().astype('str'))
+    # combin two AnnData's UMAP loc
+    adata_combined.obsm["X_umap"] = np.vstack([adata_mu_reference.obsm['X_umap'], adata_mu_query.obsm['X_umap']])
+    # clear unused categories
+    adata_combined.obs["dataset_label"] = adata_combined.obs["dataset_label"].astype('category').cat.remove_unused_categories()
+    # save reference and query low-dim .h5ad, includes .obsm["X_umap"]
+    adata_combined.write_h5ad(f"{save_path}/{reference_dataset_str}_mu{special_file_name}.h5ad")
+    print(f"Final plot dataset information: {Counter(adata_combined.obs['dataset_label'])}")
+
+    # --- plot on cell type
+
+    plot_tyser_mapping_to_datasets_attrCellType_maskTyser(adata_combined.copy(), save_path, attr="cell_typeMaskTyser",
+                                                          masked_str=test_donor, color_palette="hsv",
+                                                          legend_title="Cell type",
+                                                          reference_dataset_str=reference_dataset_str,
+                                                          special_file_str=f'_mask{test_donor}_query{test_donor}{special_file_name}', top_vis_cellType_num=15,
+                                                          )
+    plot_tyser_mapping_to_datasets_attrCellType_maskTyser(adata_combined.copy(), save_path, attr="cell_typeMask4dataset",
+                                                          masked_str='L & M & P & Z & Xiao & C', color_palette="tab20",
+                                                          legend_title="Cell type",
+                                                          reference_dataset_str=reference_dataset_str,
+                                                          special_file_str=f'_maskL&M&P&Z&Xiao&C_query{test_donor}{special_file_name}',
+                                                          query_donor=test_donor, top_vis_cellType_num=15)
+    # --- plot on dataset observed cell stage
+    plot_query_mapping_to_referenceUmapSpace_attrTimeGT(adata_combined.copy(), save_path, plot_attr='time',
+                                                        legend_title=f"Cell stage\nof Ref.",
+                                                        mask_dataset_label=test_donor,
+                                                        reference_dataset_str=reference_dataset_str,
+                                                        special_file_str=f'_cellStageOnDataset_mask{test_donor}_query{test_donor}{special_file_name}')
+    plot_query_mapping_to_referenceUmapSpace_attrTimeGT(adata_combined.copy(), save_path, plot_attr='time',
+                                                        legend_title=f"Cell stage\nof {test_donor}",
+                                                        mask_dataset_label=['L', 'M', 'P', 'Z', 'Xiao', 'C'],
+                                                        reference_dataset_str=reference_dataset_str,
+                                                        special_file_str=f'_cellStageOnDataset_maskL&M&P&Z&Xiao&C_query{test_donor}{special_file_name}')
+
+    # --- plot on time categorical
+    # plot_tyser_mapping_to_datasets_attrTimeGT(adata_combined.copy(), save_file_name, plot_attr='time',
+    #                                           query_timePoint='17.5',
+    #                                           legend_title="Cell stage",
+    #                                           mask_dataset_label=test_donor,
+    #                                           reference_dataset_str=reference_dataset_str,
+    #                                           special_file_str=f'_mask{test_donor}_query{test_donor}')
+    # plot_tyser_mapping_to_datasets_attrTimeGT(adata_combined.copy(), save_file_name, plot_attr='time',
+    #                                           query_timePoint='17.5',
+    #                                           legend_title="Cell stage",
+    #                                           mask_dataset_label="Liu & Lv & M & P & Z & Xiao & C",
+    #                                           reference_dataset_str=reference_dataset_str,
+    #                                           special_file_str=f'_maskL&M&P&Z&Xiao&C_query{test_donor}')
+    # --- plot on Predict Time
+    plot_tyser_mapping_to_4dataset_predictedTime(adata_combined.copy(), save_path, label_dic,
+                                                 mask_dataset_label=test_donor, plot_attr='predicted_time',
+                                                 reference_dataset_str=reference_dataset_str,
+                                                 special_file_str=f"_mask{test_donor}_query{test_donor}{special_file_name}"
+                                                 )
+    plot_tyser_mapping_to_4dataset_predictedTime(adata_combined.copy(), save_path, label_dic,
+                                                 mask_dataset_label='L & M & P & Z & Xiao & C',
+                                                 plot_attr='predicted_time',
+                                                 reference_dataset_str=reference_dataset_str,
+                                                 special_file_str=f"_maskL&M&P&Z&Xiao&C_query{test_donor}{special_file_name}")
+
+    # --- plot on dataset
+
+    plot_tyser_mapping_to_datasets_attrDataset(adata_combined.copy(), save_path,
+                                               attr="dataset_label", masked_str=test_donor,
+                                               color_dic={'L': '#E06377',
+                                                          'M': '#7ED957',
+                                                          'P': '#FFC947',
+                                                          'Z': '#00CED1',
+                                                          'Xiao': "#B292CA",
+                                                          'C': '#c76f00',
+                                                          # 'Lv': '#8f5239',
+                                                          test_donor: (0.9, 0.9, 0.9, 0.7)},
+                                               legend_title="Dataset",
+                                               reference_dataset_str=reference_dataset_str,
+                                               special_file_str=f"_mask{test_donor}_query{test_donor}{special_file_name}")
+    plot_tyser_mapping_to_datasets_attrDataset(adata_combined.copy(), save_path,
+                                               attr="data_type", masked_str='L & M & P & Z & Xiao & C',
+                                               color_dic={'L & M & P & Z & Xiao & C': (0.9, 0.9, 0.9, 0.7),
+                                                          test_donor: "#E06D83"},
+                                               reference_dataset_str=reference_dataset_str,
+                                               legend_title="Dataset", special_file_str=f"_maskL&M&P&Z&Xiao&C_query{test_donor}{special_file_name}")
+
+    # if umap_space_withSubsetRef:
+    #     adata_subset=from_adata_randomSelect_cells_equityTimePoint(adata_mu_reference, random_select_n_timePoint=random_select_n_timePoint, random_seed=0)
+    #     reducer = umap.UMAP(n_neighbors=umap_n_neighbors, min_dist=0.75, n_components=2, random_state=0)
+    #     adata_subset.obsm['X_umap'] = reducer.fit_transform(adata_subset.X)
+    #     adata_mu_query.obsm['X_umap'] = reducer.transform(adata_mu_query.X)
+    #     adata_combined_subMapping=adata_combined.copy()
+    #     adata_combined_subMapping.obsm["X_umap"] =  reducer.transform(adata_combined_subMapping.X)
+    #     plot_tyser_mapping_to_datasets_attrCellType_maskTyser(adata_combined_subMapping.copy(), save_path, attr="cell_typeMask4dataset",
+    #                                                           masked_str='L & M & P & Z & Xiao & C', color_palette="tab20",
+    #                                                           legend_title="Cell type",
+    #                                                           reference_dataset_str=reference_dataset_str,
+    #                                                           special_file_str=f'_maskL&M&P&Z&Xiao&C_query{test_donor}_subMapping_{special_file_name}',
+    #                                                           query_donor=test_donor)
+    #     plot_tyser_mapping_to_datasets_attrCellType_maskTyser(adata_combined_subMapping.copy(), save_path, attr="cell_typeMaskTyser",
+    #                                                           masked_str=test_donor, color_palette="hsv",
+    #                                                           legend_title="Cell type",
+    #                                                           reference_dataset_str=reference_dataset_str,
+    #                                                           special_file_str=f'_mask{test_donor}_subMapping_query{test_donor}{special_file_name}')
+    import gc
+    gc.collect()
+    return adata_mu_query
